8fd4b2431a56f29b53651e47d3e8b6ae7d386758
Kha Dinh <khadinh@g.skku.edu>
2023-12-28 05:22:13 +0000

    fix a bug in build_image.sh
    Signed-off-by: Kha Dinh <khadinh@g.skku.edu>

--- build_image.sh
+++ build_image.sh
@@ -5,10 +5,8 @@ CMD_LINE=app-helloworld.cmdl
 UNIKRAFT_DIR=../../unikraft
 BINARY=build/helloworld_qemu-x86_64
 
-if [ -f $CMD_LINE ]; then
-  echo "app-helloworld" > $CMD_LINE
+if [ ! -f $CMD_LINE ]; then
+	echo "app-helloworld" >$CMD_LINE
 fi
 
 make -j8 && sudo $UNIKRAFT_DIR/support/scripts/mkukimg -f iso -k $BINARY -b ukefi -a X64 -c $CMD_LINE -o $OUT_ISO
-
-
